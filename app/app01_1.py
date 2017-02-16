e4aac540f13dad834f35b36635d199530b0e2b63
ChunKitChung <chung@arara.com>
2017-02-16 13:52:52 +0900

    refactoring

--- app/app01_1.py
+++ app/app01_1.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request
-import redis
 
 app = Flask(__name__)
 default = '1'
-cache = redis.StrictRedis(host='redis', port=6379, db=0)
-cache.set(default, 'one-one-one')
+cache = {default: 'one-one-one'}
 
 @app.route('/', methods=['GET', 'POST']) 
 def hello():
@@ -14,11 +12,11 @@ def hello():
         key = request.form['key']
 
     if request.method == 'POST' and request.form['submit'] == 'save':
-        cache.set(key, request.form['cache_value'])
+        cache[key] = request.form['cache_value']
 
     cache_value = None
-    if cache.get(key):
-        cache_value = cache.get(key).decode('utf-8')
+    if key in cache:
+        cache_value = cache[key]
 
     return render_template('index.html', key=key, cache_value=cache_value)
 
--- app/test.py
+++ app/test.py
@@ -1,10 +1,10 @@
 import unittest
-import app05
+import app02 as app
 
 class TestDockerapp(unittest.TestCase):
 
     def setUp(self):
-        self.app = app05.app.test_client()
+        self.app = app.app.test_client()
 
     def test_save_value(self):
         response = self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
@@ -18,8 +18,6 @@ class TestDockerapp(unittest.TestCase):
         assert response.status_code == 200
         assert b'2' in response.data
         assert b'two' in response.data
-
-
 
 if __name__=='__main__':
     unittest.main()
